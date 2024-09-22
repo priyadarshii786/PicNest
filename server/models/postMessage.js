6db000ed0a23271e1985da81febfc4969cf791d9
priyadarshii786 <tathagatp918@gmail.com>
2024-09-23 03:38:53 +0530

    C-04; Revisited the previous commit ie; C-04

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -21,6 +21,6 @@ const postSchema = mongoose.Schema({
 
 });
 
-const PostMessage = mongoose.model(PostMessage, postSchema);
+const PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
