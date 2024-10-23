044794cefb8890da0983ccbf9ea2c8e706b8358e
priyadarshii786 <tathagatp918@gmail.com>
2024-10-24 00:19:06 +0530

    C-14; Changes made for the Routing and various new components has been introduced for sleek appearence of code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
-import React, {useEffect} from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import PicNest from './images/PicNest.jpg'
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
-import useStyles from './styles.js';
-
-
-function App() {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
-  return (
-    <Container maxwidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.image} variant="h2" align="center">PicNest</Typography>
-        <img className={classes.image} src={PicNest} alt="PicNest" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-
-          </Grid>
-
-        </Container>
-      </Grow>
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
-
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
